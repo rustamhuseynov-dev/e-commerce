5bf403cded578d07e74542007a9689c75ae514a8
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 23:02:44 +0400

    Update AuthController.java

--- src/main/java/com/rustam/e_commerce/controller/AuthController.java
+++ src/main/java/com/rustam/e_commerce/controller/AuthController.java
@@ -2,15 +2,13 @@ package com.rustam.e_commerce.controller;
 
 
 import com.rustam.e_commerce.dto.request.AuthRequest;
+import com.rustam.e_commerce.dto.request.RefreshTokenRequest;
 import com.rustam.e_commerce.dto.response.AuthResponse;
 import com.rustam.e_commerce.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/auth")
@@ -23,4 +21,15 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest){
         return new ResponseEntity<>(authService.login(authRequest), HttpStatus.ACCEPTED);
     }
+
+    @PostMapping(path = "/refresh-token")
+    public ResponseEntity<String> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest){
+        return new ResponseEntity<>(authService.refreshToken(refreshTokenRequest), HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping(path = "/logout")
+    public ResponseEntity<String> logout(@RequestBody RefreshTokenRequest refreshTokenRequest){
+        return new ResponseEntity<>(authService.logout(refreshTokenRequest),HttpStatus.OK);
+    }
+
 }
