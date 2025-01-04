4b3fae8f73634a293a59a2c2a06b35619a4e15f0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 22:53:48 +0400

    Create BaseUserRepository.java

---
+++ src/main/java/com/rustam/e_commerce/dao/repository/BaseUserRepository.java
@@ -0,0 +1,19 @@
+package com.rustam.e_commerce.dao.repository;
+
+import com.rustam.e_commerce.dao.entity.BaseUser;
+import com.rustam.e_commerce.dao.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BaseUserRepository extends JpaRepository<BaseUser, UUID> {
+    @Query(value = "SELECT * FROM base_users WHERE user_type = 'USER' AND username = :username", nativeQuery = true)
+    Optional<User> findByUsername(String username);
+
+    List<User> findAllBy();
+}
