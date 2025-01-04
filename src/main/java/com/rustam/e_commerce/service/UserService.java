e3f018aaccd820f848cde10762ab228d9ddb6fa3
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 23:29:06 +0400

    Update UserService.java

--- src/main/java/com/rustam/e_commerce/service/UserService.java
+++ src/main/java/com/rustam/e_commerce/service/UserService.java
@@ -1,17 +1,22 @@
 package com.rustam.e_commerce.service;
 
 
+import com.rustam.e_commerce.dao.entity.BaseUser;
 import com.rustam.e_commerce.dao.entity.User;
 import com.rustam.e_commerce.dao.entity.enums.Role;
 import com.rustam.e_commerce.dao.repository.BaseUserRepository;
 import com.rustam.e_commerce.dto.request.UserCreateRequest;
+import com.rustam.e_commerce.dto.request.UserUpdateRequest;
 import com.rustam.e_commerce.dto.response.UserCreateResponse;
 import com.rustam.e_commerce.dto.response.UserResponse;
+import com.rustam.e_commerce.dto.response.UserUpdateResponse;
+import com.rustam.e_commerce.exception.custom.ExistsException;
 import com.rustam.e_commerce.mapper.UserMapper;
 import com.rustam.e_commerce.util.UtilService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +32,7 @@ public class UserService {
     PasswordEncoder passwordEncoder;
     UtilService utilService;
     UserMapper userMapper;
+    ModelMapper modelMapper;
 
     public UserCreateResponse create(UserCreateRequest userCreateRequest) {
         User user = User.builder()
@@ -45,5 +51,20 @@ public class UserService {
     public List<UserResponse> read() {
         List<User> users = utilService.findAllBy();
         return userMapper.toResponses(users);
+    }
+
+    public UserUpdateResponse update(UserUpdateRequest userUpdateRequest) {
+        String currentUsername = utilService.getCurrentUsername();
+        BaseUser user = utilService.findById(userUpdateRequest.getId());
+        utilService.validation(currentUsername, user.getId());
+        boolean exists = utilService.findAllBy().stream()
+                .map(User::getUsername)
+                .anyMatch(existingUsername -> existingUsername.equals(userUpdateRequest.getUsername()));
+        if (exists) {
+            throw new ExistsException("This username is already taken.");
+        }
+        modelMapper.map(userUpdateRequest, user);
+        baseUserRepository.save(user);
+        return userMapper.toUpdated(user);
     }
 }
