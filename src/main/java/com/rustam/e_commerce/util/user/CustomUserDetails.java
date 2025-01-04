54e620794aa62b7dd58e5c3086c3fc11d63f9c01
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 22:55:00 +0400

    Create CustomUserDetails.java

---
+++ src/main/java/com/rustam/e_commerce/util/user/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.rustam.e_commerce.util.user;
+
+import com.rustam.e_commerce.dao.entity.BaseUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public record CustomUserDetails(BaseUser baseUser) implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return baseUser.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return baseUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return baseUser.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
