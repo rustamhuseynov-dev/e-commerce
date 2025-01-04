b559831eac77fe6093216e688da6256d2f2d8f91
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 22:53:51 +0400

    Create AuthRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/AuthRequest.java
@@ -0,0 +1,16 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AuthRequest {
+
+    private String username;
+    private String password;
+}
