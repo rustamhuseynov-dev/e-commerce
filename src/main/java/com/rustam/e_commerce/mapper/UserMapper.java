c28b892a111b0f2a664d1aa685b9f67fe2388705
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-04 23:21:11 +0400

    Create UserMapper.java

---
+++ src/main/java/com/rustam/e_commerce/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.rustam.e_commerce.mapper;
+
+import com.rustam.e_commerce.dao.entity.User;
+import com.rustam.e_commerce.dto.response.UserCreateResponse;
+import com.rustam.e_commerce.dto.response.UserResponse;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
+)
+public interface UserMapper {
+
+    UserCreateResponse toResponse(User user);
+
+    List<UserResponse> toResponses(List<User> users);
+}
