9ba92302973121f11910b7de0e81a2877a1b0c59
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-05 22:24:23 +0400

    Create AdminCreateRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/AdminCreateRequest.java
@@ -0,0 +1,24 @@
+package com.rustam.e_commerce.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdminCreateRequest {
+
+    @NotBlank(message = "The id column cannot be empty.")
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+
+}
