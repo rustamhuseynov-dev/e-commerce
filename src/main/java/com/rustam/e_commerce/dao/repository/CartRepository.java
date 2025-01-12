48581ab9ce9316118a5874cb26121f25f83c82fc
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:03:14 +0400

    Create CartRepository.java

---
+++ src/main/java/com/rustam/e_commerce/dao/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.rustam.e_commerce.dao.repository;
+
+import com.rustam.e_commerce.dao.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserId(UUID userId);
+}
