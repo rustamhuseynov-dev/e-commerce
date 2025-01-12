443002a0ef25bec942c71d7e50cf575373fef23a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:03:18 +0400

    Create EmployeeRepository.java

---
+++ src/main/java/com/rustam/e_commerce/dao/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.rustam.e_commerce.dao.repository;
+
+import com.rustam.e_commerce.dao.entity.user.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+}
