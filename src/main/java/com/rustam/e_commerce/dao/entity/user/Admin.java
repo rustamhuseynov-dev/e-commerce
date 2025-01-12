ea57bfdcfe026d04ec2d1677ac7b651a509ade32
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:03:01 +0400

    Create Admin.java

---
+++ src/main/java/com/rustam/e_commerce/dao/entity/user/Admin.java
@@ -0,0 +1,23 @@
+package com.rustam.e_commerce.dao.entity.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity
+@DiscriminatorValue("ADMIN")
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin extends BaseUser {
+
+    @Column(unique = true)
+    private String username;
+}
