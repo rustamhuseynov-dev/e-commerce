9b25e7d962def58ee6d011c6301c6f0fbf0029f8
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:03:25 +0400

    Create CartUpdateRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/CartUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartUpdateRequest {
+
+    private Long id;
+    private Long productId;
+    private Integer quantity;
+}
