386e3fa21a834a6449377401d82538b57e8e7ec5
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:03:39 +0400

    Create CartResponse.java

---
+++ src/main/java/com/rustam/e_commerce/dto/response/CartResponse.java
@@ -0,0 +1,21 @@
+package com.rustam.e_commerce.dto.response;
+
+import com.rustam.e_commerce.dao.entity.CartItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CartResponse {
+
+    private Long userId;
+    private List<CartItem> items;
+    private double totalPrice;
+    private String message;
+}
