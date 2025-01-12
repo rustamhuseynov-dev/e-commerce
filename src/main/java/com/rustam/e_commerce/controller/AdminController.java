0660c328b3c9237f7c404aa4408560867a4482d6
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:02:25 +0400

    Update AdminController.java

--- src/main/java/com/rustam/e_commerce/controller/AdminController.java
+++ src/main/java/com/rustam/e_commerce/controller/AdminController.java
@@ -2,8 +2,6 @@ package com.rustam.e_commerce.controller;
 
 import com.rustam.e_commerce.dto.request.AdminCreateRequest;
 import com.rustam.e_commerce.dto.request.AdminUpdateRequest;
-import com.rustam.e_commerce.dto.request.UserCreateRequest;
-import com.rustam.e_commerce.dto.request.UserUpdateRequest;
 import com.rustam.e_commerce.dto.response.*;
 import com.rustam.e_commerce.service.AdminService;
 import jakarta.validation.Valid;
@@ -11,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 import java.util.UUID;
 
 @RestController
