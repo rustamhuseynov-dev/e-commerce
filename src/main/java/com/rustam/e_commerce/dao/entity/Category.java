9fae9692f74ff3d4807d2bdc13d1128fd563117a
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-12 17:02:43 +0400

    Create Category.java

---
+++ src/main/java/com/rustam/e_commerce/dao/entity/Category.java
@@ -0,0 +1,34 @@
+package com.rustam.e_commerce.dao.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", cascade =  CascadeType.ALL )
+    private List<Product> products;
+
+}
