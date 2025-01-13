95e335ebbc3dd8b87a22228a635d1b5ce9b066d0
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-13 21:39:43 +0400

    Update Employee.java

--- src/main/java/com/rustam/e_commerce/dao/entity/user/Employee.java
+++ src/main/java/com/rustam/e_commerce/dao/entity/user/Employee.java
@@ -4,47 +4,19 @@ import com.rustam.e_commerce.dao.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDate;
-import java.util.Set;
-import java.util.UUID;
 
-@Entity
-@Table(name = "employees")
+
+@EqualsAndHashCode(callSuper = true)
 @Data
+@Entity
+@DiscriminatorValue("EMPLOYEE")
 @AllArgsConstructor
 @NoArgsConstructor
 @SuperBuilder
-public class Employee{
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    UUID id;
-    String name;
-    String surname;
-    String phone;
-    @Column(unique = true)
-    String email;
-    String password;
-    Boolean enabled;
+public class Employee extends BaseUser{
     String username;
     LocalDate startWorkDay;
     String iban;
-
-    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
-    @JoinTable(name = "authorities", joinColumns = @JoinColumn(name = "base_user_id"))
-    @Column(name = "role", nullable = false)
-    @Enumerated(EnumType.STRING)
-    Set<Role> authorities;
-
-    public String getId() {
-        if (id != null) {
-            return id.toString();
-        }
-        return null;
-    }
 }
