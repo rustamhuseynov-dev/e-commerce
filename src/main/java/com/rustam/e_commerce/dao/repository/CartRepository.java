3c1be1dd608fc6ad67a9c2c421664d652583b3e5
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-13 21:21:54 +0400

    Update CartRepository.java

--- src/main/java/com/rustam/e_commerce/dao/repository/CartRepository.java
+++ src/main/java/com/rustam/e_commerce/dao/repository/CartRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    Optional<Cart> findByUserId(UUID userId);
+    Optional<Cart> findByUser(UUID userId);
 }
