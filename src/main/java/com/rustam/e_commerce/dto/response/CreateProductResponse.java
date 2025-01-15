469f96b068c647ba71abd388c6a35d030d5dab62
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-15 10:48:14 +0400

    Create CreateProductResponse.java

---
+++ src/main/java/com/rustam/e_commerce/dto/response/CreateProductResponse.java
@@ -0,0 +1,23 @@
+package com.rustam.e_commerce.dto.response;
+
+import com.rustam.e_commerce.dao.entity.Category;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateProductResponse {
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+    private Category category;
+}
