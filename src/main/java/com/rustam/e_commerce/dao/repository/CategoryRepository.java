fd321fef681337dfb8f45f498d8332bb5e7601a9
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-15 10:48:00 +0400

    Create CategoryRepository.java

---
+++ src/main/java/com/rustam/e_commerce/dao/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.rustam.e_commerce.dao.repository;
+
+import com.rustam.e_commerce.dao.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+}
