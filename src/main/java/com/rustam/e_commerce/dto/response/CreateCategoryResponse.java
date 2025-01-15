089efa65306d37b0981ab6b30d68d0605cc33e47
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-15 10:48:11 +0400

    Create CreateCategoryResponse.java

---
+++ src/main/java/com/rustam/e_commerce/dto/response/CreateCategoryResponse.java
@@ -0,0 +1,14 @@
+package com.rustam.e_commerce.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateCategoryResponse {
+    private String categoryName;
+}
