cc48f29b4b69476cd790e7eff7de049a1a6af081
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-16 10:28:19 +0400

    Create ProductDeleteResponse.java

---
+++ src/main/java/com/rustam/e_commerce/dto/response/ProductDeleteResponse.java
@@ -0,0 +1,21 @@
+package com.rustam.e_commerce.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductDeleteResponse {
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+    private String text;
+}
