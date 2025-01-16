4ea90f33fbd0a808204ecffaff1be8da3018bcbf
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-16 10:15:47 +0400

    Create ProductUpdateResponse.java

---
+++ src/main/java/com/rustam/e_commerce/dto/response/ProductUpdateResponse.java
@@ -0,0 +1,23 @@
+package com.rustam.e_commerce.dto.response;
+
+import com.rustam.e_commerce.dao.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductUpdateResponse {
+    private Long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+    private Category category;
+    private String userId;
+}
