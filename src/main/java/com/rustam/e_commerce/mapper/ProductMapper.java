10ce411faae4d34138f35616419f34dc0486f491
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-16 10:15:51 +0400

    Update ProductMapper.java

--- src/main/java/com/rustam/e_commerce/mapper/ProductMapper.java
+++ src/main/java/com/rustam/e_commerce/mapper/ProductMapper.java
@@ -3,6 +3,7 @@ package com.rustam.e_commerce.mapper;
 import com.rustam.e_commerce.dao.entity.Product;
 import com.rustam.e_commerce.dto.response.CreateProductResponse;
 import com.rustam.e_commerce.dto.response.ProductReadResponse;
+import com.rustam.e_commerce.dto.response.ProductUpdateResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -20,4 +21,6 @@ public interface ProductMapper {
     CreateProductResponse toResponse(Product product);
 
     List<ProductReadResponse> toResponses(List<Product> products);
+
+    ProductUpdateResponse toUpdateResponse(Product product);
 }
