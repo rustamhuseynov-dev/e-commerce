3c6c3d79139add514751c1d4e491b9bd7763e358
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-16 10:39:25 +0400

    Create ProductReadRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/ProductReadRequest.java
@@ -0,0 +1,17 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProductReadRequest {
+    private String userId;
+    private Long productId;
+}
