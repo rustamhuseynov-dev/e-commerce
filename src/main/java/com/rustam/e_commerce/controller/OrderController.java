3bb1686712d20ed811feb37ab1e75f59d62d1a4e
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-19 23:14:06 +0400

    Create OrderController.java

---
+++ src/main/java/com/rustam/e_commerce/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.rustam.e_commerce.controller;
+
+import com.rustam.e_commerce.dto.request.OrderCreateRequest;
+import com.rustam.e_commerce.dto.response.OrderCreateResponse;
+import com.rustam.e_commerce.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<OrderCreateResponse> create(@RequestBody OrderCreateRequest orderCreateRequest){
+        return new ResponseEntity<>(orderService.create(orderCreateRequest), HttpStatus.CREATED);
+    }
+}
