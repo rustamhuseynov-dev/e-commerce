d86be4b900d132acc30fa5952940b085530c6a4f
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-19 23:30:45 +0400

    Update OrderService.java

--- src/main/java/com/rustam/e_commerce/service/OrderService.java
+++ src/main/java/com/rustam/e_commerce/service/OrderService.java
@@ -1,79 +1,74 @@
 package com.rustam.e_commerce.service;
 
-import com.rustam.e_commerce.dao.entity.Cart;
+import com.rustam.e_commerce.dao.entity.*;
+import com.rustam.e_commerce.dao.entity.enums.OrderStatus;
 import com.rustam.e_commerce.dao.repository.OrderRepository;
 import com.rustam.e_commerce.dto.request.OrderCreateRequest;
 import com.rustam.e_commerce.dto.response.OrderCreateResponse;
+import com.rustam.e_commerce.mapper.OrderMapper;
+import com.rustam.e_commerce.util.UtilService;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class OrderService {
 
-    private final OrderRepository orderRepository;
+    OrderRepository orderRepository;
+    UtilService utilService;
+    OrderMapper orderMapper;
 
     public OrderCreateResponse create(OrderCreateRequest orderCreateRequest) {
-        Cart cart = cartRepo.findCartByEmailAndCartId(emailId, cartId);
+        Cart cart = utilService.findByCartId(orderCreateRequest.getCartId());
 
-        if (cart == null) {
-            throw new ResourceNotFoundException("Cart", "cartId", cartId);
-        }
+        Order order = Order.builder()
+                .orderDate(LocalDate.now())
+                .orderStatus(OrderStatus.SUCCESSFUL)
+                .totalAmount(cart.getTotalPrice())
+                .email(orderCreateRequest.getEmail())
+                .build();
 
-        Order order = new Order();
-
-        order.setEmail(emailId);
-        order.setOrderDate(LocalDate.now());
-
-        order.setTotalAmount(cart.getTotalPrice());
-        order.setOrderStatus("Order Accepted !");
-
-        Payment payment = new Payment();
-        payment.setOrder(order);
-        payment.setPaymentMethod(paymentMethod);
-
-        payment = paymentRepo.save(payment);
-
-        order.setPayment(payment);
-
-        Order savedOrder = orderRepo.save(order);
+        orderRepository.save(order);
 
         List<CartItem> cartItems = cart.getCartItems();
 
-        if (cartItems.size() == 0) {
-            throw new APIException("Cart is empty");
-        }
-
         List<OrderItem> orderItems = new ArrayList<>();
 
-        for (CartItem cartItem : cartItems) {
-            OrderItem orderItem = new OrderItem();
-
-            orderItem.setProduct(cartItem.getProduct());
-            orderItem.setQuantity(cartItem.getQuantity());
-            orderItem.setDiscount(cartItem.getDiscount());
-            orderItem.setOrderedProductPrice(cartItem.getProductPrice());
-            orderItem.setOrder(savedOrder);
-
-            orderItems.add(orderItem);
-        }
-
-        orderItems = orderItemRepo.saveAll(orderItems);
-
-        cart.getCartItems().forEach(item -> {
-            int quantity = item.getQuantity();
-
-            Product product = item.getProduct();
-
-            cartService.deleteProductFromCart(cartId, item.getProduct().getProductId());
-
-            product.setQuantity(product.getQuantity() - quantity);
-        });
-
-        OrderDTO orderDTO = modelMapper.map(savedOrder, OrderDTO.class);
-
-        orderItems.forEach(item -> orderDTO.getOrderItems().add(modelMapper.map(item, OrderItemDTO.class)));
-
-        return orderDTO;
+//        for (CartItem cartItem : cartItems) {
+//            OrderItem orderItem = new OrderItem();
+//
+//            orderItem.setProduct(cartItem.getProduct());
+//            orderItem.setQuantity(cartItem.getQuantity());
+//            orderItem.setDiscount(cartItem.getDiscount());
+//            orderItem.setOrderedProductPrice(cartItem.getProductPrice());
+//            orderItem.setOrder(savedOrder);
+//
+//            orderItems.add(orderItem);
+//        }
+//
+//        orderItems = orderItemRepo.saveAll(orderItems);
+//
+//        cart.getCartItems().forEach(item -> {
+//            int quantity = item.getQuantity();
+//
+//            Product product = item.getProduct();
+//
+//            cartService.deleteProductFromCart(cartId, item.getProduct().getProductId());
+//
+//            product.setQuantity(product.getQuantity() - quantity);
+//        });
+//
+//        OrderDTO orderDTO = modelMapper.map(savedOrder, OrderDTO.class);
+//
+//        orderItems.forEach(item -> orderDTO.getOrderItems().add(modelMapper.map(item, OrderItemDTO.class)));
+//
+        return orderMapper.toDto(order);
     }
 }
