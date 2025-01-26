c52e9854981d98e5d2007686f11fb439df72be68
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-01-26 17:40:32 +0400

    Create UpdateCategoryRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/UpdateCategoryRequest.java
@@ -0,0 +1,15 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateCategoryRequest {
+    private Long categoryId;
+    private String categoryName;
+}
