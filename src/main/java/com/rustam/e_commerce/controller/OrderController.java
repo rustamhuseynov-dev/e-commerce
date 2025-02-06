693c1d7b50c4e101225e4c33a1a7b53ca01433d2
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-06 14:45:10 +0400

    Update OrderController.java

--- src/main/java/com/rustam/e_commerce/controller/OrderController.java
+++ src/main/java/com/rustam/e_commerce/controller/OrderController.java
@@ -2,15 +2,15 @@ package com.rustam.e_commerce.controller;
 
 import com.rustam.e_commerce.dto.request.OrderCreateRequest;
 import com.rustam.e_commerce.dto.response.OrderCreateResponse;
+import com.rustam.e_commerce.dto.response.OrderReadResponse;
 import com.rustam.e_commerce.service.OrderService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/order")
@@ -23,4 +23,10 @@ public class OrderController {
     public ResponseEntity<OrderCreateResponse> create(@Valid @RequestBody OrderCreateRequest orderCreateRequest){
         return new ResponseEntity<>(orderService.create(orderCreateRequest), HttpStatus.CREATED);
     }
+
+    @GetMapping(path = "/read-order")
+    public ResponseEntity<List<OrderReadResponse>> read(){
+        return new ResponseEntity<>(orderService.read(),HttpStatus.OK);
+    }
+-
 }
