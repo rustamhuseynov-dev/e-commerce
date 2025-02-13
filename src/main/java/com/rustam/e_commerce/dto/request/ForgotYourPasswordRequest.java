c1646f24f44fe4f06b82fdee5415cf87450f2977
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-13 22:00:26 +0400

    Create ForgotYourPasswordRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/ForgotYourPasswordRequest.java
@@ -0,0 +1,15 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ForgotYourPasswordRequest {
+
+    private String email;
+}
