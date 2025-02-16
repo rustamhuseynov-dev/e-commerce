d01f8a831af184364c07d1a26cc55240e481225b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-17 00:09:07 +0400

    Create FavoriteService.java

---
+++ src/main/java/com/rustam/e_commerce/service/FavoriteService.java
@@ -0,0 +1,41 @@
+package com.rustam.e_commerce.service;
+
+import com.rustam.e_commerce.dao.entity.Category;
+import com.rustam.e_commerce.dao.entity.Favorite;
+import com.rustam.e_commerce.dao.entity.Product;
+import com.rustam.e_commerce.dao.repository.FavoriteRepository;
+import com.rustam.e_commerce.dto.request.AddToFavoriteRequest;
+import com.rustam.e_commerce.dto.response.AddToFavoriteResponse;
+import com.rustam.e_commerce.mapper.FavoriteMapper;
+import com.rustam.e_commerce.util.UtilService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+public class FavoriteService {
+
+    FavoriteRepository favoriteRepository;
+    UtilService utilService;
+    FavoriteMapper favoriteMapper;
+    public AddToFavoriteResponse addToFavorite(AddToFavoriteRequest addToFavoriteRequest) {
+        String userId = utilService.getCurrentUsername();
+        Product product = utilService.findByProductId(addToFavoriteRequest.getProductId());
+        Category category = utilService.findByCategoryId(product.getCategoryId());
+        Favorite favorite = Favorite.builder()
+                .productId(product.getProductId())
+                .productName(product.getProductName())
+                .price(product.getPrice())
+                .quantity(product.getQuantity())
+                .description(product.getDescription())
+                .userId(userId)
+                .categoryId(product.getCategoryId())
+                .categoryName(category.getCategoryName())
+                .build();
+        favoriteRepository.save(favorite);
+        return favoriteMapper.toDto(favorite);
+    }
+}
