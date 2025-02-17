5264e51ca076ec21828d6c50c81838b206856248
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-17 17:36:38 +0400

    Create ReadFavoritesRequest.java

---
+++ src/main/java/com/rustam/e_commerce/dto/request/ReadFavoritesRequest.java
@@ -0,0 +1,14 @@
+package com.rustam.e_commerce.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReadFavoritesRequest {
+    private String userId;
+}
