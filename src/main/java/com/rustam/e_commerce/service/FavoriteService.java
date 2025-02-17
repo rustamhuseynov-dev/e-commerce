098287cfd18ef4a6a095de743e33ae13b93e56e6
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-17 17:36:46 +0400

    Update FavoriteService.java

--- src/main/java/com/rustam/e_commerce/service/FavoriteService.java
+++ src/main/java/com/rustam/e_commerce/service/FavoriteService.java
@@ -5,13 +5,17 @@ import com.rustam.e_commerce.dao.entity.Favorite;
 import com.rustam.e_commerce.dao.entity.Product;
 import com.rustam.e_commerce.dao.repository.FavoriteRepository;
 import com.rustam.e_commerce.dto.request.AddToFavoriteRequest;
+import com.rustam.e_commerce.dto.request.ReadFavoritesRequest;
 import com.rustam.e_commerce.dto.response.AddToFavoriteResponse;
+import com.rustam.e_commerce.dto.response.ReadFavoritesResponse;
 import com.rustam.e_commerce.mapper.FavoriteMapper;
 import com.rustam.e_commerce.util.UtilService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -37,5 +41,10 @@ public class FavoriteService {
                 .build();
         favoriteRepository.save(favorite);
         return favoriteMapper.toDto(favorite);
+    }
+
+    public List<ReadFavoritesResponse> readFavorites(ReadFavoritesRequest readFavoritesRequest) {
+        List<Favorite> userFavorites = favoriteRepository.findAllByUserId(readFavoritesRequest.getUserId());
+        return favoriteMapper.toDtos(userFavorites);
     }
 }
