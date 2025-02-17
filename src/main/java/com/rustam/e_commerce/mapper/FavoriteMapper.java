d1768748e48fde86214710ebe96bc73fc35252df
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-17 17:36:43 +0400

    Update FavoriteMapper.java

--- src/main/java/com/rustam/e_commerce/mapper/FavoriteMapper.java
+++ src/main/java/com/rustam/e_commerce/mapper/FavoriteMapper.java
@@ -2,9 +2,12 @@ package com.rustam.e_commerce.mapper;
 
 import com.rustam.e_commerce.dao.entity.Favorite;
 import com.rustam.e_commerce.dto.response.AddToFavoriteResponse;
+import com.rustam.e_commerce.dto.response.ReadFavoritesResponse;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -15,4 +18,6 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 )
 public interface FavoriteMapper {
     AddToFavoriteResponse toDto(Favorite favorite);
+
+    List<ReadFavoritesResponse> toDtos(List<Favorite> userFavorites);
 }
