a13ff7ae1cf223d9a5d800c29829fc27e9d1fa66
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-19 13:42:50 +0400

    Update FavoriteRepository.java

--- src/main/java/com/rustam/e_commerce/dao/repository/FavoriteRepository.java
+++ src/main/java/com/rustam/e_commerce/dao/repository/FavoriteRepository.java
@@ -5,7 +5,10 @@ import com.rustam.e_commerce.dao.entity.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavoriteRepository extends JpaRepository<Favorite,Long> {
     List<Favorite> findAllByUserId(String userId);
+
+    Optional<Favorite> findByProductId(Long productId);
 }
