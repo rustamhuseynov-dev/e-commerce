84fc2a388dcccb74bc22f2102317b9d674c94d4c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-22 20:43:09 +0400

    Update TrackOrderResponse.java

--- src/main/java/com/rustam/e_commerce/dto/response/TrackOrderResponse.java
+++ src/main/java/com/rustam/e_commerce/dto/response/TrackOrderResponse.java
@@ -1,19 +1,27 @@
 package com.rustam.e_commerce.dto.response;
 
 import com.rustam.e_commerce.dao.entity.enums.OrderStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class TrackOrderResponse {
-    private OrderStatus orderStatus;
-    private String productName;
-    private Integer quantity;
-    private Double totalAmount;
-    private String shippingAddress;
+    private String trackingNumber;
+
+    private String location;
+
+    private OrderStatus status;
+
+    private Long orderId;
+
+    private LocalDateTime updatedAt;
 }
