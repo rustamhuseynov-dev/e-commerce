ee2e55b24952e73135a9167b1503f816163e6ba9
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-24 00:18:44 +0400

    Update EmailAndPasswordUpdateRequest.java

--- src/main/java/com/rustam/e_commerce/dto/request/EmailAndPasswordUpdateRequest.java
+++ src/main/java/com/rustam/e_commerce/dto/request/EmailAndPasswordUpdateRequest.java
@@ -16,7 +16,7 @@ import java.util.UUID;
 @Builder
 public class EmailAndPasswordUpdateRequest {
 
-    @NotNull(message = "The user id column cannot be empty.")
+    @NotBlank(message = "The user id column cannot be empty.")
     private UUID userId;
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.")
     @NotNull
