1c6bd634e02373c426d3a1ffda77aa3689cf1239
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-26 17:48:33 +0400

    Update CreateCategoryRequest.java

--- src/main/java/com/rustam/e_commerce/dto/request/CreateCategoryRequest.java
+++ src/main/java/com/rustam/e_commerce/dto/request/CreateCategoryRequest.java
@@ -1,6 +1,8 @@
 package com.rustam.e_commerce.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,5 +14,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CreateCategoryRequest {
     @NotBlank(message = "You cannot submit an empty category name.")
+    @Pattern(regexp = "^[a-zA-Z0-9 _-]{1,20}$", message = "Category name contains invalid characters!")
     private String categoryName;
 }
