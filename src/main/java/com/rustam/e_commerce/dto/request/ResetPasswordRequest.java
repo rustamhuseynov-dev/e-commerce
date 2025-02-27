20fbd3c630e677e98de93e3d4e0bf29ab511aa8b
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-27 09:32:24 +0400

    Update ResetPasswordRequest.java

--- src/main/java/com/rustam/e_commerce/dto/request/ResetPasswordRequest.java
+++ src/main/java/com/rustam/e_commerce/dto/request/ResetPasswordRequest.java
@@ -1,5 +1,7 @@
 package com.rustam.e_commerce.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ResetPasswordRequest {
-
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.")
     private String email;
+    @NotBlank(message = "The password cannot be empty.")
     private String newPassword;
+    @NotBlank(message = "enter new password here")
     private String againPassword;
 }
