2fc7cbc586839799d3c9502d1f29432e6eac7cfe
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-27 09:32:20 +0400

    Update EmailVerificationRequest.java

--- src/main/java/com/rustam/e_commerce/dto/request/EmailVerificationRequest.java
+++ src/main/java/com/rustam/e_commerce/dto/request/EmailVerificationRequest.java
@@ -1,5 +1,8 @@
 package com.rustam.e_commerce.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +13,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class EmailVerificationRequest {
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Enter a valid email address.")
     private String email;
+    @NotBlank(message = "You must send the verification code")
     private String verificationCode;
 }
