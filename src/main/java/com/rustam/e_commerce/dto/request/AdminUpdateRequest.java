c0232646d2913c039377ff8f0919054c4d60ac3c
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-02-27 09:23:10 +0400

    Update AdminUpdateRequest.java

--- src/main/java/com/rustam/e_commerce/dto/request/AdminUpdateRequest.java
+++ src/main/java/com/rustam/e_commerce/dto/request/AdminUpdateRequest.java
@@ -2,6 +2,7 @@ package com.rustam.e_commerce.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,9 +15,15 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class AdminUpdateRequest {
+    @Size(max = 20, message = "Name cannot be longer than 20 characters.")
     private String name;
+
+    @Size(max = 20, message = "Surname cannot be longer than 20 characters.")
     private String surname;
+
+    @Pattern(regexp = "^[a-zA-Z0-9 _-]{0,20}$", message = "Username contains invalid characters or is too long!")
     private String username;
-    @Pattern(regexp = "\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "The phone number must be in the format (000)-000-0000.")
+
+    @Pattern(regexp = "^$|\\(\\d{3}\\)-\\d{3}-\\d{4}", message = "The phone number must be in the format (000)-000-0000.")
     private String phone;
 }
