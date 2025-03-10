74241bea21d6e40a5ddbb745a3eefda099e50af2
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-03-10 18:15:51 +0400

    Update ProductRepository.java

--- src/main/java/com/rustam/e_commerce/dao/repository/ProductRepository.java
+++ src/main/java/com/rustam/e_commerce/dao/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import com.rustam.e_commerce.dao.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByCategoryId(Long categoryId);
 }
