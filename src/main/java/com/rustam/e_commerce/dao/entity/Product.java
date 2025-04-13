7d0262d9aca23b124426562e2621bface8a21a9e
rustamhuseynov <147059960+rustamhuseynov-dev@users.noreply.github.com>
2025-04-13 20:27:01 +0400

    Update Product.java

--- src/main/java/com/rustam/e_commerce/dao/entity/Product.java
+++ src/main/java/com/rustam/e_commerce/dao/entity/Product.java
@@ -1,24 +1,11 @@
 package com.rustam.e_commerce.dao.entity;
 
+import com.rustam.e_commerce.dao.entity.campaign.Campaign;
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.*;
 
 @Entity
 @Table(name = "products")
@@ -46,6 +33,10 @@ public class Product {
 
     @JoinColumn(name = "category_id")
     private Long categoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "campaign_id")
+    private Campaign campaign;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     @ToString.Exclude // Sonsuz rekursiyanın qarşısını alır
